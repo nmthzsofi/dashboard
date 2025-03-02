1721eae56aaadb38012259e3902c4968fc14fd00
nmthzsofi <168550167+nmthzsofi@users.noreply.github.com>
2025-03-02 15:54:35 +0100

    init

---
+++ src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Welcome to your Dashboard!</h2>
+      {/* You can add more habit-tracking components or data here */}
+    </div>
+  );
+}
+
+export default Dashboard;
